fde7be90b7dd9e7ff9ddb39f3f225f0c510a2de4
KONSTANTYN YVASHCHENKO <civaschenko@yahoo.com>
2017-12-19 19:38:35 +0200

    create a special file to put all  text quotes

---
+++ phrases.py
@@ -0,0 +1,78 @@
+START_INTRODUCTION = """
+    You are in Ukraine one year before the presidential elections
+    and you want to take this post through elections or another way.
+    You agree at least get post of premier ministre too, for begin.
+    You have few possibilities but some variants are maybe.
+
+    You can:
+     - "To achieve this goal, we must seek compromises.
+        Politics is the art of the possible." ,
+
+     - "I do not need money! I do not want to depend on anyone.
+        I can do it without money."
+
+    What the strategy you will choice?
+"""
+COMPROMISES = """
+    Every cloud has a silver lining!
+    You must pay 3 millions bucks for part in race!
+    Look for money, bro and "jump on the bandwagon"!
+    We could do with the extra cash, Bro )).
+"""
+NO_MONEY = """
+    Actions speak louder than words!
+    No money no honey!
+"""
+MONEY_INTRODUCTION = """
+    It's big money, brother!
+    You have three options today.
+
+    You can conclude an agreement with foreign partners
+    and promise the development of democracy in the country.
+    Then you will be helped to create your new party
+    and join the consolidated opposition.
+
+    The second case is when you take money from both
+    the oligarch and foreign partners at the same time,
+    but they do not know about each other.
+    Then you make your own political party to go your own way.
+
+    Thirdly. Circumstances are such that you will be able to agree
+    with only one of the oligarchs to create a new political party.
+    Do you promise to service me at least 5 years
+    and lobby his interests in parliament.
+"""
+ANTI_CORRUPTION = """
+    Two weeks later...
+    The Anti-Corruption Bureau starts an investigation
+    and you are a suspect. You are at home arrested on bail.
+    "A hot potato".
+"""
+REVOLUTION_INTRODUCTION = """
+    You made a deal with radical nationalists and made a revolution.
+    Currently, there is no government in the country.
+
+    Perhaps you will want to use this situation and get power.
+    Will you seek help from the military?
+
+    Or will you determine the date of the early election?
+"""
+JOKES = [
+    "Add insult to injury.",
+    "Bite off more than you can chew.",
+    "Can't judge a book by its cover.",
+    "Cry over spilt milk.",
+    "Curiosity killed the cat",
+]
+INAUGURATION = """
+    #############################
+    ##### Ta-da-a! You won! #####
+    ### Now You are PRESIDENT! ##
+    ### \_|$|_/**\$/**\_|$|_/ ###
+    #############################
+"""
+PREMIER_MINISTRE = [
+    "A blessing in disguise.",
+    "With a lousy lamb, at least with a coat of wool.",
+    "It takes two to tango.",
+]
