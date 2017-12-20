528aa1d4c4d4a68a9a241bfec4f6b5b5c0d9bf82
KONSTANTYN YVASHCHENKO <civaschenko@yahoo.com>
2017-12-20 22:51:18 +0200

    added some phrases and try make class StateMachine

--- phrases.py
+++ phrases.py
@@ -5,11 +5,11 @@ START_INTRODUCTION = """
     You have few possibilities but some variants are maybe.
 
     You can:
-     - "To achieve this goal, we must seek compromises.
-        Politics is the art of the possible." ,
+    [1] To achieve this goal, we must seek compromises.
+        Politics is the art of the possible. ,
 
-     - "I do not need money! I do not want to depend on anyone.
-        I can do it without money."
+    [2] I do not need money! I do not want to depend on anyone.
+        I can do it without money.
 
     What the strategy you will choice?
 """
@@ -30,17 +30,17 @@ MONEY_INTRODUCTION = """
     You can conclude an agreement with foreign partners
     and promise the development of democracy in the country.
     Then you will be helped to create your new party
-    and join the consolidated opposition.
+    and join the consolidated opposition.(enter "1")
 
     The second case is when you take money from both
     the oligarch and foreign partners at the same time,
     but they do not know about each other.
-    Then you make your own political party to go your own way.
+    Then you make your own political party to go your own way.(enter "2")
 
     Thirdly. Circumstances are such that you will be able to agree
     with only one of the oligarchs to create a new political party.
     Do you promise to service me at least 5 years
-    and lobby his interests in parliament.
+    and lobby his interests in parliament.(enter "3")
 """
 ANTI_CORRUPTION = """
     Two weeks later...
@@ -50,12 +50,17 @@ ANTI_CORRUPTION = """
 """
 REVOLUTION_INTRODUCTION = """
     You made a deal with radical nationalists and made a revolution.
-    Currently, there is no government in the country.
+	Currently, there is no government in the country.
 
-    Perhaps you will want to use this situation and get power.
-    Will you seek help from the military?
+	[1] You might want to use this situation 
+		and get power in your hands right away. 
+		You can declare yourself President.
+		Will you seek help from the military?
 
-    Or will you determine the date of the early election?
+	[2] Or do you determine the date of early elections?
+		To make everything happen democratically.
+
+	What will be your choice?
 """
 JOKES = [
     "Add insult to injury.",
@@ -78,3 +83,23 @@ PREMIER_MINISTRE = [
     "Let sleeping dogs lie!",
     "Piece of cake!",
 ]
+WITH_OPPO_TO_1ST_ELECT = """
+    In a joint union, your party and other oppositionists, 
+    you go to the polls. There are all chances to win 
+    if the campaign is properly conducted.
+"""
+DEAL_WITH_RADICALS = """
+    You have money already and are determined. 
+    You are sure that the people will support you 
+    and the radical-minded militants will help 
+    make the revolution. Forward! 
+    Now a favorable situation - the current 
+    government has a very low support rating.
+"""
+DICTATOR = """
+    You became the President thanks to the support 
+    of the military, but this did not support the world community. 
+    You were called dictator and the country fell under sanctions. 
+    Your closest supporters and yesterday's friends betrayed you. 
+    You fled the country.
+"""
--- presrace.py
+++ presrace.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python3
 #-*- coding:utf-8 -*-
 """
-*Map
- -next_state
- -opening_state
-*Engine
- -play
-*State
- - enter
- *Money
- *FirstElection
- *SecondElection
- *Loser
- *President
- *Premier Ministre
- *Revolution
 
 give money
 get money
@@ -47,40 +33,54 @@ class Start(State):
     def enter(self):
         print(textwrap.dedent(phrases.START_INTRODUCTION))
         choice = input("--> ")
-        if choice == "We must seek compromises.":
+        if choice == "1":
             print(textwrap.dedent(phrases.COMPROMISES))
             return "Money"
-        elif choice == "I can do it without money!":
+        elif choice == "2":
             print(textwrap.dedent(phrases.NO_MONEY))
             return "Loser"
         else:
             print("DOESN'T COMPUTE!")
-            return start_state()
+            return "Start"
 
 class Money(State):
 	def enter():
 		print(textwrap.dedent(phrases.MONEY_INTRODUCTION))
 		choice = input("--> ")
-		if choice == "join the consolidated opposition":
+		if choice == "1":
+		    print(textwrap.dedent(phrases.WITH_OPPO_TO_1ST_ELECT))
+		    return "First_Election"
+		elif choice == "2":
 		    print(textwrap.dedent(phrases.ANTI_CORRUPTION))
-		    return "Loser"
-		elif choice == "No":
-		    print(textwrap.dedent())
-		    return "Loser"
+		    return "Revolution"
+		elif choice == "3":
+		    print(textwrap.dedent(phrases.DEAL_WITH_RADICALS))
+		    return "Loser" 
 		else:
 		    print("DOESN'T COMPUTE!")
-		    return enter()
+		    return "Money"
 
-def Revolution():
+class Revolution(State):
+	def enter(self):
+		print(textwrap.dedent(phrases.REVOLUTION_INTRODUCTION))
+		choice = input("--> ")
+		if choice == "1":
+			print(textwrap.dedent(phrases.WITH_OPPO_TO_1ST_ELECT))
+			return "First_Election"
+		elif choice == "2":
+			print(textwrap.dedent(phrases.DICTATOR))
+			return "Loser"
+		else:
+		    print("DOESN'T COMPUTE!")
+		    return "Revolution"
+
+class FirstElection(State):
     pass
 
-def FirstElection():
+class SecondElection(State):
     pass
 
-def SecondElection():
-    pass
-
-class Loser():
+class Loser(State):
     def enter(self):
         jokes = phrases.JOKES
         joke = random.choice(jokes)
@@ -88,12 +88,12 @@ class Loser():
         print(joke)
         print("-" * len(joke))
         print("\tYou lose!" * 3)
-        exit(1)
+        return "Finish"
 
-def President():
+class President(State):
     def enter(self):
         print(textwrap.dedent(phrases.INAUGURATION))
-        exit(1)
+        return "Finish"
 
 class PremierMinistre(State):
     def enter(self):
@@ -102,56 +102,51 @@ class PremierMinistre(State):
         print("#" * len(joke))
         print(joke)
         print("#" * len(joke))
-        exit(1)
+        return "Finish"
 
+class Finish(State):
+	def enter(self):
+		print("You won!")
+		exit(1)
 
-class Map(object):
+class StateMachine(object):
+    """
+    This is a map of major milestones and game handlers.
+    In the dictionary, all class handlers are accessible by the key.
+    In the list of final states only those handlers on which the game ends.
+    """	    
+    def __init__(self, name):
+        self.startState = name  ## Start
+        self.handlers = {
+        "Start": Start(),
+        "Money": Money(),
+        "Revolution": Revolution(),
+        "First_Election": FirstElection(),
+        "Second_Election": SecondElection(),
+        "Loser": Loser(),
+        "President": President(),
+        "Premier_Ministre": PremierMinistre(),
+        "Finish": Finish()
+    	}
+        self.endState = self.handlers["Finish"]  ## <__main__.Finish object at 0x7fa610760940>
 
-	        handlers = {
-		        "Start": Start(),
-		        "Money": Money(),
-		        "Revolution": Revolution(),
-		        "First_Election": FirstElection(),
-		        "Second_Election": SecondElection(),
-		        "Loser": Loser(),
-		        "President": President(),
-		        "Premier_Ministre": PremierMinistre(),
-		        "Finish": Finish()
-		    }
-		    
-    def __init__(self, start):
-        """
-        This is a map of major milestones and game handlers.
-        In the dictionary, all class handlers are accessible by the key.
-        In the list of final states only those handlers on which the game ends.
-        """
-        self.start = start
+    def set_start(self):
+        return    self.handlers[self.startState]
 
+    def next_state(self, return_key):
+        return    self.handlers[return_key]
+        
+    def run(self, name):
+        currentState = next_state(name)
+        while True:
+            self.next_state(currentState.enter())
+            currentState.enter()
+            if currentState == self.endState:
+                break
+        
 
-    def next_state(self, new_state):
-        self.new_state = new_state
-        state = self.handlers[self.new_state]
-        return state.enter()
-
-#    def opening_state(self):
-#        return state.enter()
-
-class Engine(object):
-    def __init__(self, state_map):
-        self.state_map = state_map  #  self.state_map = <__main__.Map object at 0x7f0d60d25908>
-    def play(self):
-        current_state = self.state_map.next_state()
-        print("current_state: {}".format(current_state))
-        last_state = self.scene_map.endStates
-        print("last_state: {}".format(last_state))
-        while current_state not in last_state:
-            next_state_name = current_scene.enter()
-            current_state = self.state_map.next_state(next_scene_name)
-
-            current_state.enter()
-
-
-a_map = Map('Start')
-a_map.next_state(newState)
-a_game = Engine(a_map)
-a_game.play()
+m = StateMachine("Start")
+m.set_start()
+#n = m.next_state(s)
+#print("n", n)
+#m.run(n)
